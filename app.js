9611bcde1d15db6f8ac22f53dc5a286bd8278767
sempiternus666 <50131158+sempiternus666@users.noreply.github.com>
2019-10-21 09:39:58 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-window.addEventListener('load')
+window.addEventListener('load', ()=>{
+    /* let long = 24.7037952;
+    let lat = 59.4075648; */
+    let long;
+    let lat;
+
+    if("geolocation" in navigator){
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+        console.log("Long: ", long);
+        console.log("Lat: ", lat);
+    }
+
+    /* if(navigator.geolocation){
+        navigator-geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            console.log("Long: ", long);
+            console.log("lat: ", lat);
+
+        })
+    }
+ */
+});
