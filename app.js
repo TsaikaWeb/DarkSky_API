4fa8ba274a29cd7a5b4aff71095210ac9b492ebd
sempiternus666 <50131158+sempiternus666@users.noreply.github.com>
2019-10-21 09:57:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', ()=>{
-    /* let long = 24.7037952;
-    let lat = 59.4075648; */
-    let long;
-    let lat;
+    let long = 24.7037952;
+    let lat = 59.4075648;
+  /*   let long;
+    let lat; */
 
-    if("geolocation" in navigator){
+  /*   if("geolocation" in navigator){
         long = position.coords.longitude;
         lat = position.coords.latitude;
         console.log("Long: ", long);
         console.log("Lat: ", lat);
     }
-
+ */
     /* if(navigator.geolocation){
         navigator-geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
@@ -21,4 +21,14 @@ window.addEventListener('load', ()=>{
         })
     }
  */
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api= `${proxy}https://api.darksky.net/forecast/69fb9727b4fbc09bc9578c7228de41ca/${lat},${long}`;
+    
+    fetch(api)
+    .then(response => {
+       return response.json(); 
+    });
+
+
+
 });
